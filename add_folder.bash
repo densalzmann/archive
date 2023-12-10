e186684fb161afcba6999e28ef7c92ce649ab375
Dennis Salzmann <dennis.salzmann@dfki.de>
2023-12-10 16:55:58 +0100

    feat: add folder to archive

--- add_folder.bash
+++ add_folder.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rel_path=$1
-mkdir -p $rel_path && touch $rel_path/.placeholder
-echo "!$rel_path/.placeholder" >> .gitignore
-echo $path added
+mkdir -p $rel_path && touch $rel_path/.gitignore
+echo "*
+!.gitignore" >> $rel_path/.gitignore
+echo $rel_path added
